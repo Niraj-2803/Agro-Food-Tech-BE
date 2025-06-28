09439e751fd82686b24395f173bbe8c64f76836d
Niraj-2803 <nirajtupsundar28@gmail.com>
2025-06-28 22:41:34 +0530

    Query model update

--- models/queryModel.js
+++ models/queryModel.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 const querySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  contact: { type: String, required: true },
-  query: { type: String, required: true }
+  fullName: { type: String, required: true },
+  emailAddress: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  companyName: { type: String },
+  interestedIn: {
+    type: String,
+    enum: ['WholeSale Purchase', 'Retail Information', 'Distribution Partnership', 'Other'],
+    required: true,
+  },
+  message: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Query', querySchema);
